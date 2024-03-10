6f9d1d4d1c9cec1983cab009d801fec179afc4ca
Ahmadal26 <Almusallam296@hotmail.com>
2024-03-10 15:03:56 +0300

    Startup code"

---
+++ src/test/java/tailorApp1/tailorApp1/TailorApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tailorApp1.tailorApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
